2006feba7e32d0f6410ac33f82eed6b213dca264
Ruilin Yan <ruilin.yan000928@gmail.com>
2026-05-22 11:24:37 -0700

    Added configuration for newly imported tasks

--- MFR_benchmark/isaac_lab_tasks/__init__.py
+++ MFR_benchmark/isaac_lab_tasks/__init__.py
@@ -1,4 +1,5 @@
 """Isaac Lab task registrations for MFR benchmark."""
 
 from . import screwdriver_turning  # noqa: F401
+from . import screwdriver_turning_360  # noqa: F401
 from . import screwdriver_manipulation  # noqa: F401
