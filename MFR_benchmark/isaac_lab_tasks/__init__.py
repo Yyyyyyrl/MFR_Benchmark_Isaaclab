d5e860686a0e7cbb5827bac0d6b00a9457389c40
Ruilin Yan <ruilin.yan000928@gmail.com>
2026-06-03 18:11:39 -0700

    Append task list

--- MFR_benchmark/isaac_lab_tasks/__init__.py
+++ MFR_benchmark/isaac_lab_tasks/__init__.py
@@ -2,5 +2,6 @@
 
 from . import screwdriver_turning  # noqa: F401
 from . import screwdriver_turning_360  # noqa: F401
+from . import screwdriver_continuous_turning  # noqa: F401
 from . import screwdriver_turning_linker_hand  # noqa: F401
 from . import screwdriver_manipulation  # noqa: F401
